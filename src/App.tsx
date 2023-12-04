450dcba34f7318d3f4b33000b2c49f134c0ed48c
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2023-12-04 15:15:10 +0300

    => [ 10-11 ]

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,62 @@
 import React from 'react'
 import './App.css'
 
+// Components
+
+// images
+import myimage from './images/content-iamge.webp'
+
 const App = () => {
     return (
-        <div className="App">
-            samurai way
+        <div className="app-wrapper">
+
+            <header className={'header'}>
+                <img alt={'sry'} src={'#'}/>
+            </header>
+
+            <nav className={'nav'}>
+                <div>
+                    <a href={'#'} target={'_blank'}>Profile</a>
+                </div>
+                <div>
+                    <a href={'#'} target={'_blank'}>Messages</a>
+                </div>
+                <div>
+                    <a href={'#'} target={'_blank'}>News</a>
+                </div>
+                <div>
+                    <a href={'#'} target={'_blank'}>Music</a>
+                </div>
+                <div>
+                    <a href={'#'} target={'_blank'}>Settings</a>
+                </div>
+
+            </nav>
+
+            <main className={'content'}>
+                <div>
+                    <img alt={'sry'} src={myimage}/>
+                </div>
+                <div>
+                    avatar+description
+                </div>
+                <div>
+                    my posts
+                    <div>
+                        New Post
+                    </div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
+
+            </main>
+
         </div>
     )
 }
 export default App
+
