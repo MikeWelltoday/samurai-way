c92c5baa25a53e0a3a0944e87f4377f1a76dde88
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-01-18 07:10:05 +0300

    => saved

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,20 +9,20 @@ import './Navbar.styles.css'
 
 export const Navbar: FC = () => {
     return (
-        <nav className={'nav'}>
-            <div className={'nav__item'}>
+        <nav className="nav">
+            <div className="nav__item">
                 <a href={'#'} target={'_blank'}>Profile</a>
             </div>
-            <div className={'nav__item'}>
+            <div className="nav__item">
                 <a href={'#'} target={'_blank'}>Messages</a>
             </div>
-            <div className={'nav__item'}>
+            <div className="nav__item">
                 <a href={'#'} target={'_blank'}>News</a>
             </div>
-            <div className={'nav__item'}>
+            <div className="nav__item">
                 <a href={'#'} target={'_blank'}>Music</a>
             </div>
-            <div className={'nav__item'}>
+            <div className="nav__item">
                 <a href={'#'} target={'_blank'}>Settings</a>
             </div>
 
