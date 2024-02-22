f5d2103ac4e4b89064bfe14f492f53d9a4ca7d66
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-02-22 10:03:04 +0300

    => saved

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import {BrowserRouter} from 'react-router-dom'
 import {store} from './redux/redux-store'
-import {Provider} from './StoreContext'
+import {Provider} from 'react-redux'
 
 //=============================================================================
 
--- src/layout/main/dialogs/DialogsContainer.tsx
+++ src/layout/main/dialogs/DialogsContainer.tsx
@@ -2,6 +2,7 @@ import React, {FC} from 'react'
 import {Dialogs} from './Dialogs'
 import {dialogsReducerAddMessageAC, dialogsReducerUpdateNewMessageBodyAC} from '../../../redux/dialogs-reducer'
 import {StoreContext} from '../../../StoreContext'
+import {connect} from 'react-redux'
 
 //========================================================================================
 // 🎲 .T.Y.P.E.S.
@@ -35,7 +36,27 @@ export const DialogsContainer: FC<DialogsContainerPropsType> = (props) => {
                 )
             }}
         </StoreContext.Consumer>
-
-
     )
 }
+
+//data
+function mapStateToProps(state: any) {
+    return {
+        dialogsPage: state.dialogsPage
+    }
+}
+
+// functions
+function mapDispatchToProps() {
+    return {
+        c: 3
+    }
+}
+
+
+// вызываем функцию, она возвращает функцию,
+// и вызываем функцию которую вернули
+
+// в аругмент помещаем чистую компоненту, которую хотим обернуть
+// автоматически снабжает компоненту стором
+const SuperDialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs)
