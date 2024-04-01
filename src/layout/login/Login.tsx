8319c77b85df8d80fa8b42b141138c5711280167
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-04-01 13:51:12 +0300

    => saved

--- src/layout/login/Login.tsx
+++ src/layout/login/Login.tsx
@@ -5,8 +5,22 @@ import S from './Login.module.css'
 
 export const Login = () => {
     return (
-        <div className={S.login}>
-            LOGIN
+        <div className={S.loginForm}>
+            <form>
+                <div className={S.formGroup}>
+                    <label htmlFor="login">LOGIN</label>
+                    <input type="text" id="login" name="login"/>
+                </div>
+                <div className={S.formGroup}>
+                    <label htmlFor="password">PASSWORD</label>
+                    <input type="password" id="password" name="password"/>
+                </div>
+                <div className={`${S.formGroup} ${S.lastFormGroup}`}>
+                    <input type="checkbox" id="rememberMe" name="rememberMe"/>
+                    <label htmlFor="rememberMe">REMEMBER ME</label>
+                </div>
+                <button type="submit" className={S.loginButton}>IN</button>
+            </form>
         </div>
     )
 }
